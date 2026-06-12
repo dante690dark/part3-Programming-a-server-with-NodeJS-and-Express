26a22611dd50e9ab499f22aa3ed46533b51b4043
Alexander <dante690dark@gmail.com>
2026-06-11 22:28:16 -0500

    add create, get and delete functionality

--- phonebook/index.js
+++ phonebook/index.js
@@ -24,12 +24,19 @@ const persons = [
   },
 ];
 
+const generateID = () => {
+  let number;
+  do {
+    number = Math.floor(Math.random() * 9).toString();
+  } while (persons.some((person) => person.id === number));
+
+  return number;
+};
+
+app.use(express.json());
+
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
-});
-
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
 });
 
 app.get("/info", (request, response) => {
@@ -39,6 +46,35 @@ app.get("/info", (request, response) => {
       <p>${new Date()}</p>
     </div>
   `);
+});
+
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.find((note) => note.id === id);
+
+  person ? response.json(person) : response.status(404).end();
+});
+
+app.post("/api/person", (request, response) => {
+  const { name, number } = request.body;
+
+  if (!request.body)
+    return response.status(400).json({ error: "content missing" });
+
+  const person = {
+    name,
+    number,
+    id: generateID(),
+  };
+
+  response.json(person);
+});
+
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = persons.filter((person) => person.id !== id);
+
+  response.status(204).end();
 });
 
 const PORT = 3001;
